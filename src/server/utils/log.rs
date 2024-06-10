e4bb39f0682625a7fed799dcc2a165590804774c
Voltaged <140754373+VoltagedDebunked@users.noreply.github.com>
2024-06-10 13:59:34 -0500

    Created log.rs

---
+++ src/server/utils/log.rs
@@ -0,0 +1,21 @@
+use log::{info, warn, error, debug};
+
+pub fn init_logger() {
+    env_logger::init();
+}
+
+pub fn log_info(message: &str) {
+    info!("{}", message);
+}
+
+pub fn log_warn(message: &str) {
+    warn!("{}", message);
+}
+
+pub fn log_error(message: &str) {
+    error!("{}", message);
+}
+
+pub fn log_debug(message: &str) {
+    debug!("{}", message);
+}
