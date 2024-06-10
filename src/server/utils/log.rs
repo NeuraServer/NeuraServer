4af8cc74469f5ac5f3560a411cc008134990dee7
Voltaged <140754373+VoltagedDebunked@users.noreply.github.com>
2024-06-10 15:04:58 -0500

    Updated log.rs

--- src/server/utils/log.rs
+++ src/server/utils/log.rs
@@ -1,7 +1,10 @@
-use log::{info, warn, error, debug};
+use log::{info, warn, error, debug, trace, LevelFilter};
+use env_logger::{Env, Builder};
+use std::io::Write;
 
-pub fn init_logger() {
-    env_logger::init();
+pub fn init_logger(level: LevelFilter) {
+    let mut builder = Builder::from_env(Env::default().default_filter_or("info"));
+    builder.filter(None, level).init();
 }
 
 pub fn log_info(message: &str) {
@@ -18,4 +21,22 @@ pub fn log_error(message: &str) {
 
 pub fn log_debug(message: &str) {
     debug!("{}", message);
+}
+
+pub fn log_trace(message: &str) {
+    trace!("{}", message);
+}
+
+pub fn set_custom_format() {
+    env_logger::Builder::from_default_env()
+        .format(|buf, record| {
+            writeln!(
+                buf,
+                "{} [{}] - {}",
+                chrono::Local::now().format("%Y-%m-%d %H:%M:%S"),
+                record.level(),
+                record.args()
+            )
+        })
+        .init();
 }
