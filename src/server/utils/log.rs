041da1f7f39b6dcc73fef4c6417b5d4f0b985f36
Voltaged <140754373+VoltagedDebunked@users.noreply.github.com>
2024-06-10 15:06:15 -0500

    Updated log.rs

--- src/server/utils/log.rs
+++ src/server/utils/log.rs
@@ -1,6 +1,7 @@
 use log::{info, warn, error, debug, trace, LevelFilter};
 use env_logger::{Env, Builder};
 use std::io::Write;
+use chrono::Local;
 
 pub fn init_logger(level: LevelFilter) {
     let mut builder = Builder::from_env(Env::default().default_filter_or("info"));
@@ -33,10 +34,46 @@ pub fn set_custom_format() {
             writeln!(
                 buf,
                 "{} [{}] - {}",
-                chrono::Local::now().format("%Y-%m-%d %H:%M:%S"),
+                Local::now().format("%Y-%m-%d %H:%M:%S"),
                 record.level(),
                 record.args()
             )
         })
         .init();
+}
+
+pub fn log_to_file(log_file: &str) {
+    let file = std::fs::OpenOptions::new()
+        .create(true)
+        .write(true)
+        .append(true)
+        .open(log_file)
+        .unwrap();
+
+    env_logger::Builder::new()
+        .format(|buf, record| {
+            writeln!(
+                buf,
+                "{} [{}] - {}",
+                Local::now().format("%Y-%m-%d %H:%M:%S"),
+                record.level(),
+                record.args()
+            )
+        })
+        .target(env_logger::Target::Pipe(Box::new(file)))
+        .init();
+}
+
+pub fn set_max_log_level(level: LevelFilter) {
+    log::set_max_level(level);
+}
+
+pub fn log_custom(level: LevelFilter, message: &str) {
+    match level {
+        LevelFilter::Error => log_error(message),
+        LevelFilter::Warn => log_warn(message),
+        LevelFilter::Info => log_info(message),
+        LevelFilter::Debug => log_debug(message),
+        LevelFilter::Trace => log_trace(message),
+    }
 }
